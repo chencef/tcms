fac95570933a12531471d07149c60c2edf0ca6a4
chencef <chencef@gmail.com>
2025-10-03 11:00:09 +0800

    test1

---
+++ src/cmd.bat
@@ -0,0 +1,4 @@
+cloc-1.64.exe common vos/common vos/posix --by-file-by-lang  --report-file=report.txt
+cloc-1.64.exe vos/posix --by-file-by-lang  --report-file=report_posix.txt
+cloc-1.64.exe vos/windows --by-file-by-lang  --report-file=report_windows.txt
+cloc-1.64.exe vos/vxworks --by-file-by-lang  --report-file=report_vxworks.txt
